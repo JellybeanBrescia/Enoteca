7a0a65079f425a0818921ca90260edce7bc006ba
AndreaPenazzi <andrea.penazzi.it@ieee.org>
2017-05-04 16:32:25 +0200

    Magazzino aggiunto

---
+++ src/magazzino/Magazzino.java
@@ -0,0 +1,60 @@
+package magazzino;
+
+import java.util.Vector;
+
+public class Magazzino {
+	private String name;
+	private double prezzo;
+	private int data;
+	private String località;
+	private String produttore;
+	private String valuta;
+	private int cont;
+	private Vector<Vino> vino;
+	private Vector<String> nomeVino;
+	
+	
+	public  Magazzino(Vector<Vino> vino){
+		this.vino=vino;
+		
+		
+		 
+	}
+	
+	
+	public void ListaVini(String name){
+		for(int i=0;i<vino.size();i++){
+			String tmp=vino.get(i).getName();
+			nomeVino.add(tmp);
+		}
+		for(int i=0;i<vino.size();i++){
+			System.out.println(vino.get(i).getName());
+		}
+		
+	}
+	
+	public void QuantBottVino(String name,int cont){
+		for(int i=0;i<vino.size();i++){
+			String tmp=vino.get(i).getName()+vino.get(i).getCont();
+			nomeVino.add(tmp);
+		}
+		for(int i=0;i<vino.size();i++){
+			System.out.println(vino.get(i).getName()+":"+vino.get(i).getCont());
+		}
+	
+		
+	}
+	
+	public void QuantBottProduttore(String produttore,int cont){
+		for(int i=0;i<vino.size();i++){
+			String tmp=vino.get(i).getProduttore()+vino.get(i).getCont();
+			nomeVino.add(tmp);
+		}
+		for(int i=0;i<vino.size();i++){
+			System.out.println(vino.get(i).getProduttore()+":"+vino.get(i).getCont());
+		}
+		
+	}
+	
+
+}
---
+++ src/magazzino/Vino.java
@@ -0,0 +1,76 @@
+package magazzino;
+
+public class Vino {
+	private String name;
+	private double prezzo;
+	private int data;
+	private String località;
+	private String produttore;
+	private String valuta;
+	private int cont;
+	
+	//COSTRUTTORI
+	public Vino(String name, double prezzo, int data, String località, String produttore, String valuta, int cont) {
+		super();
+		this.name = name;
+		this.prezzo = prezzo;
+		this.data = data;
+		this.località = località;
+		this.produttore = produttore;
+		this.valuta = valuta;
+		this.cont = cont;
+	}
+	public Vino() {
+		super();
+		
+	}
+	
+	//GETTESR AND SETTERS
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getPrezzo() {
+		return prezzo;
+	}
+	public void setPrezzo(double prezzo) {
+		this.prezzo = prezzo;
+	}
+	public int getData() {
+		return data;
+	}
+	public void setData(int data) {
+		this.data = data;
+	}
+	public String getLocalità() {
+		return località;
+	}
+	public void setLocalità(String località) {
+		this.località = località;
+	}
+	public String getProduttore() {
+		return produttore;
+	}
+	public void setProduttore(String produttore) {
+		this.produttore = produttore;
+	}
+	public String getValuta() {
+		return valuta;
+	}
+	public void setValuta(String valuta) {
+		this.valuta = valuta;
+	}
+	public int getCont() {
+		return cont;
+	}
+	public void setCont(int cont) {
+		this.cont = cont;
+	}	
+	public String toString(){
+		 String tmp = "Il " +name+ " di valore " +prezzo+valuta+ " prodotto nel " +data+ " in " +località+ " da " +produttore+ " in quantità di " +cont+ "bottiglie";
+		 return tmp;
+	}
+	
+}
