79d0745ea36818040791b664775f5460feb980d7
n4rwal <r.kumar002@studenti.unibs.it>
2017-05-04 16:30:01 +0200

    vino update

--- src/Vino.java
+++ src/Vino.java
@@ -1,20 +1,22 @@
-
 public class Vino {
-	String name;
-	double prezzo;
-	int data;
-	String località;
-	String produttore;
-	String valuta;
+	private String name;
+	private double prezzo;
+	private int data;
+	private String località;
+	private String produttore;
+	private String valuta;
+	private int cont;
 	
 	//COSTRUTTORI
-	public Vino(String name, double prezzo, int data, String località, String produttore) {
+	public Vino(String name, double prezzo, int data, String località, String produttore, String valuta, int cont) {
 		super();
 		this.name = name;
 		this.prezzo = prezzo;
 		this.data = data;
 		this.località = località;
 		this.produttore = produttore;
+		this.valuta = valuta;
+		this.cont = cont;
 	}
 	public Vino() {
 		super();
@@ -25,7 +27,6 @@ public class Vino {
 	public String getName() {
 		return name;
 	}
-	
 	public void setName(String name) {
 		this.name = name;
 	}
@@ -53,7 +54,21 @@ public class Vino {
 	public void setProduttore(String produttore) {
 		this.produttore = produttore;
 	}
-	
-	
+	public String getValuta() {
+		return valuta;
+	}
+	public void setValuta(String valuta) {
+		this.valuta = valuta;
+	}
+	public int getCont() {
+		return cont;
+	}
+	public void setCont(int cont) {
+		this.cont = cont;
+	}	
+	public String toString(){
+		 String tmp = "Il " +name+ " di valore " +prezzo+valuta+ " prodotto nel " +data+ " in " +località+ " da " +produttore+ " in quantità di " +cont+ "bottiglie";
+		 return tmp;
+	}
 	
 }
